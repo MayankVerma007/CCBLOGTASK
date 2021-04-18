be802554e016a85c7cf00d42669b8ceaaebb8f33
MayankVerma007 <mail2mayank2003@gmail.com>
2021-04-18 14:12:00 +0530

    commit message

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 #SECRET_KEY = '7lzv#3088o!$mopbq+&a61bl5p4-&=qxza+2in=rydl=px7x5%'
-SECRET_KEY = os.eniron.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
