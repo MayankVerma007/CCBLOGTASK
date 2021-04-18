746b3237cd5ecc5e2ac24aff14a58013e41fd43d
MayankVerma007 <mail2mayank2003@gmail.com>
2021-04-18 13:53:55 +0530

    commit message

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '7lzv#3088o!$mopbq+&a61bl5p4-&=qxza+2in=rydl=px7x5%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['mayank-cc-blog.herokuapp.com']
+ALLOWED_HOSTS = ['mayank-cc-blog.herokuapp.com','127.0.0.1']
 
 
 # Application definition
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -138,3 +139,16 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+
+AWS_S3_REGION_NAME = "us-east-2"
+
+AWS_S3_SIGNATURE_VERSION = "s3v4"
+
+
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
